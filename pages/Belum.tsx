33c2cbb1bad45bf3781b9584e26157b2db08a14b
Brian42565 <117646730+Brian42565@users.noreply.github.com>
2022-11-20 15:28:48 +0700

    Add files via upload
    add jobs di personalities, add function untuk calculate cf di Belum.tsx, gtw cara ngambil value dari radio masukin kedalam function sama keluarin hasil di sudah.tsx

--- pages/Belum.tsx
+++ pages/Belum.tsx
@@ -8,7 +8,7 @@ import {
   VStack,
   Button,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Navbar from "../Components/Navbar";
 import Link from "next/link";
 
@@ -50,6 +50,9 @@ const IntroEx = (props: any) => {
         "Bertemu dengan orang lain dan mengikuti aktivitas sosial melelahkan.",
     },
   ];
+
+  // const [value, setValue] = useState<string[]>([])
+
   return (
     <Flex
       bgGradient={useColorModeValue(
@@ -311,88 +314,88 @@ const Result = () => {
   interface person {
     id: number;
     name: string;
-    job: string;
+    job: string[];
   }
   const Personalities: person[] = [
     {
       id: 1,
       name: "INTJ",
-      job: "",
+      job: ["manager proyek", "auditor", "arsitek", "software developer", "desainer", "animator", "konsultan bisnis", "teknisi mesin", "teknisi sipil"],
     },
     {
       id: 2,
       name: "INTP",
-      job: "",
+      job: ["peneliti", "dosen", "dokter", "astronomi", "ahli ekonomi", "pengamat politik", "penulis", "software developer", "aktuaria", "arkeolog", "system analyst"],
     },
     {
       id: 3,
       name: "ENTJ",
-      job: "",
+      job: ["manager finansial", "advokat", "manager bisnis", "wirausaha", "purchasing", "sales", "teknisi sipil", "akuntan", "hakim"],
     },
     {
       id: 4,
       name: "ENTP",
-      job: "",
+      job: ["politikus", "sales", "aktor", "musisi", "reporter", "sejarahwan", "ahli sosial", "produser", "desainer", "advokat", "detektif"],
     },
     {
       id: 5,
       name: "INFJ",
-      job: "",
+      job: ["perawat", "guru sekolah", "psikiatri", "pemuka agama", "holistik", "pekerja sosial", "customer service", "mediator", "penulis", "manager HRD"],
     },
     {
       id: 6,
       name: "INFP",
-      job: "",
+      job: ["film editor", "fotografer", "chiropractor", "psikolog", "HR specialist", "dokter anak-anak", "kurator", "pustakawan", "psikiatri", "editor film"],
     },
     {
       id: 7,
       name: "ENFJ",
-      job: "",
+      job: ["edukator kesehatan", "bimbingan konseling", "film editor", "manager PR", "reporter", "dosen", "guru sekolah", "manager marketing", "sales", "pramugari", "trainer", "produser", "sosiolog"],
     },
     {
       id: 8,
       name: "ENFP",
-      job: "",
+      job: ["aktor", "musisi", "market analyst", "sales", "manager marketing", "pramugari", "penata rambut", "sosiolog", "pekerja sosial", "reporter", "penulis", "desainer", "guru sekolah"],
     },
     {
       id: 9,
       name: "ISTJ",
-      job: "",
+      job: ["akuntan", "ahli logistik", "manager finasial", "purchasing", "document administrator", "sales", "arsitek", "ahli agrikultur", "teknisi sipil", "teknisi mesin", "ahli geologi", "teknisi kehutanan", "operator mesin", "atlit"],
     },
     {
       id: 10,
       name: "ISFJ",
-      job: "",
+      job: ["pekerja sosial", "ahli agrikultur", "forensik", "trainer", "dokter", "perawat", "apoteker", "dokter anak-anak", "guru sekolah", "pustakawan", "customer service", "pembimbing konseling", "pemadam kebakaran"],
     },
     {
       id: 11,
       name: "ESTJ",
-      job: "",
+      job: ["manager marketing", "sales", "credit analyst", "staff hotel", "petugas militer", "polisi", "jaksa", "koki", "hakim", "apoteker", "dokter gigi", "database administrator"],
     },
     {
       id: 12,
       name: "ESFJ",
-      job: "",
+      job: ["guru sekolah", "pembimbing konseling", "polisi", "legal", "dokter", "perawat", "trainer", "purchasing", "manager HRD", "sales", "manager bisnis", "reporter"],
     },
     {
       id: 13,
       name: "ISTP",
-      job: "",
+      job: ["teknisi mesin", "arsitek", "teknisi kehutanan", "fotografer", "security analyst", "software developer", "software tester", "ahli ekonomi", "pilot", "petugas militer", "pemadam kebakaran"],
     },
     {
       id: 14,
       name: "ISFP",
-      job: "",
+      job: ["desainer", "koki", "teknisi mesin", "teknisi perhutanan", "perawat", "trainer", "manager bisnis", "apoteker", "dokter hewan", "ahli botani", "polisi", "penerjemah", "ahli koreografi"],
     },
     {
       id: 15,
       name: "ESTP",
-      job: "",
+      job: ["inspektor bangunan", "teknisi mesin", "wirausaha", "trainer", "manager marketing", "manager hotel", "petugas militer", "pilot", "pramugari", "bartender", "manager properti", "developer tanah"],
     },
     {
       id: 16,
       name: "ESFP",
-      job: "",
+      job: ["perawat", "guru sekolah", "pekerja sosial", "manager retail", "purchasing", "sales", "pramugari", "koki", "desainer", "aktor" ,"fotografer", "polisi", "event organizer", "staff restoran", "ahli koreografi"],
     },
   ];
   return (
@@ -422,6 +425,68 @@ const Result = () => {
     </>
   );
 };
+
+function calculateCF(id:string[], radioValue:string[]){
+  interface answer{
+    cfExpert: number;
+    cfUser: number;
+    cfAns: number;
+  }
+  var answers: answer[] = [];
+  for(let i=0; i<5; i++){
+    var ans: answer = {
+      cfExpert: 0,
+      cfUser: 0,
+      cfAns: 0,
+    };
+
+    if(radioValue[i] == "1"){
+      ans.cfUser = 0.0;
+    }else if(radioValue[i] == "2"){
+      ans.cfUser = 0.25;
+    }else if(radioValue[i] == "3"){
+      ans.cfUser = 0.5;
+    }else if(radioValue[i] == "4"){
+      ans.cfUser = 0.75;
+    }else if(radioValue[i] == "5"){
+      ans.cfUser = 1.0;
+    }
+
+    if(id[i] == "p1"){
+      ans.cfExpert = 1.0;
+    }else if(id[i] == "p2"){
+      ans.cfExpert = 0.8;
+    }else if(id[i] == "p3"){
+      ans.cfExpert = 0.6;
+    }else if(id[i] == "p4"){
+      ans.cfExpert = 0.4;
+    }else if(id[i] == "p5"){
+      ans.cfExpert = 0.2;
+    }
+
+    ans.cfAns = ans.cfUser * ans.cfExpert;
+
+    answers.push()
+  }
+  
+  var cfOld: number = 0;
+  var cfCombine: number = 0;
+  
+  for(let i=0; i<5; i++){
+    if(i=0){
+      cfOld = answers[i].cfAns;
+    }else{
+      cfCombine = cfOld + answers[i].cfAns * (1 - cfOld);
+      cfOld = cfCombine;
+    }
+  }
+
+  return cfCombine;
+  /*
+  if cfCombine >= 0.5 then i or s or t or p
+  else then e or n or f or j 
+  */
+}
 
 const Belum = () => {
   const [state, setState] = useState("IntroEx");
--- pages/Sudah.tsx
+++ pages/Sudah.tsx
@@ -6,123 +6,95 @@ import {
   Button,
   Text,
   Select,
-  FormControl,
-  Box,
 } from "@chakra-ui/react";
 import { TriangleDownIcon } from "@chakra-ui/icons";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
 
 const Sudah = () => {
-  const [isButtonLoading, setIsButtonLoading] = useState(false);
-  const [hasil, setHasil] = useState();
-
   interface person {
     id: number;
     name: string;
-    job: string;
+    job: string[];
   }
-
-  const onSubmit = async (data: any) => {
-    try {
-      setIsButtonLoading(true);
-      const formData = new FormData();
-      formData.append("name", data.name);
-      console.log(data);
-      setHasil(data.name);
-      setIsButtonLoading(false);
-    } catch (err: any) {
-      setIsButtonLoading(false);
-      console.log(err);
-    }
-  };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<person>();
-
   const Personalities: person[] = [
     {
       id: 1,
       name: "INTJ",
-      job: "",
+      job: ["manager proyek", "auditor", "arsitek", "software developer", "desainer", "animator", "konsultan bisnis", "teknisi mesin", "teknisi sipil"],
     },
     {
       id: 2,
       name: "INTP",
-      job: "",
+      job: ["peneliti", "dosen", "dokter", "astronomi", "ahli ekonomi", "pengamat politik", "penulis", "software developer", "aktuaria", "arkeolog", "system analyst"],
     },
     {
       id: 3,
       name: "ENTJ",
-      job: "",
+      job: ["manager finansial", "advokat", "manager bisnis", "wirausaha", "purchasing", "sales", "teknisi sipil", "akuntan", "hakim"],
     },
     {
       id: 4,
       name: "ENTP",
-      job: "",
+      job: ["politikus", "sales", "aktor", "musisi", "reporter", "sejarahwan", "ahli sosial", "produser", "desainer", "advokat", "detektif"],
     },
     {
       id: 5,
       name: "INFJ",
-      job: "",
+      job: ["perawat", "guru sekolah", "psikiatri", "pemuka agama", "holistik", "pekerja sosial", "customer service", "mediator", "penulis", "manager HRD"],
     },
     {
       id: 6,
       name: "INFP",
-      job: "",
+      job: ["film editor", "fotografer", "chiropractor", "psikolog", "HR specialist", "dokter anak-anak", "kurator", "pustakawan", "psikiatri", "editor film"],
     },
     {
       id: 7,
       name: "ENFJ",
-      job: "",
+      job: ["edukator kesehatan", "bimbingan konseling", "film editor", "manager PR", "reporter", "dosen", "guru sekolah", "manager marketing", "sales", "pramugari", "trainer", "produser", "sosiolog"],
     },
     {
       id: 8,
       name: "ENFP",
-      job: "",
+      job: ["aktor", "musisi", "market analyst", "sales", "manager marketing", "pramugari", "penata rambut", "sosiolog", "pekerja sosial", "reporter", "penulis", "desainer", "guru sekolah"],
     },
     {
       id: 9,
       name: "ISTJ",
-      job: "",
+      job: ["akuntan", "ahli logistik", "manager finasial", "purchasing", "document administrator", "sales", "arsitek", "ahli agrikultur", "teknisi sipil", "teknisi mesin", "ahli geologi", "teknisi kehutanan", "operator mesin", "atlit"],
     },
     {
       id: 10,
       name: "ISFJ",
-      job: "",
+      job: ["pekerja sosial", "ahli agrikultur", "forensik", "trainer", "dokter", "perawat", "apoteker", "dokter anak-anak", "guru sekolah", "pustakawan", "customer service", "pembimbing konseling", "pemadam kebakaran"],
     },
     {
       id: 11,
       name: "ESTJ",
-      job: "",
+      job: ["manager marketing", "sales", "credit analyst", "staff hotel", "petugas militer", "polisi", "jaksa", "koki", "hakim", "apoteker", "dokter gigi", "database administrator"],
     },
     {
       id: 12,
       name: "ESFJ",
-      job: "",
+      job: ["guru sekolah", "pembimbing konseling", "polisi", "legal", "dokter", "perawat", "trainer", "purchasing", "manager HRD", "sales", "manager bisnis", "reporter"],
     },
     {
       id: 13,
       name: "ISTP",
-      job: "",
+      job: ["teknisi mesin", "arsitek", "teknisi kehutanan", "fotografer", "security analyst", "software developer", "software tester", "ahli ekonomi", "pilot", "petugas militer", "pemadam kebakaran"],
     },
     {
       id: 14,
       name: "ISFP",
-      job: "",
+      job: ["desainer", "koki", "teknisi mesin", "teknisi perhutanan", "perawat", "trainer", "manager bisnis", "apoteker", "dokter hewan", "ahli botani", "polisi", "penerjemah", "ahli koreografi"],
     },
     {
       id: 15,
       name: "ESTP",
-      job: "",
+      job: ["inspektor bangunan", "teknisi mesin", "wirausaha", "trainer", "manager marketing", "manager hotel", "petugas militer", "pilot", "pramugari", "bartender", "manager properti", "developer tanah"],
     },
     {
       id: 16,
       name: "ESFP",
-      job: "",
+      job: ["perawat", "guru sekolah", "pekerja sosial", "manager retail", "purchasing", "sales", "pramugari", "koki", "desainer", "aktor" ,"fotografer", "polisi", "event organizer", "staff restoran", "ahli koreografi"],
     },
   ];
   return (
@@ -137,43 +109,22 @@ const Sudah = () => {
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <VStack justifyContent={"center"} alignItems={"center"} spacing={5}>
+        <VStack>
           <Text>Apa hasil tes kepribadian kamu?</Text>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Box>
-              <Select
-                {...register("name", {
-                  required: "Kepribadian harap dipilih",
-                })}
-                icon={<TriangleDownIcon />}
-                variant={"outline"}
-                placeholder={"Kepribadian"}
-              >
-                {Personalities.map((i: any) => (
-                  <>
-                    <option key={i.id} value={i.name}>
-                      {i.name}
-                    </option>
-                  </>
-                ))}
-              </Select>
-              {errors.name !== undefined && (
-                <Text textColor={"red"}>{errors.name.message}</Text>
-              )}
-            </Box>
-            <Flex p={5} justifyContent={"center"} alignItems={"center"}>
-              {isButtonLoading === true ? (
-                <Button isLoading w={100} borderRadius={"999px"} type="submit">
-                  SUBMIT
-                </Button>
-              ) : (
-                <Button w={100} borderRadius={"999px"} type="submit">
-                  SUBMIT
-                </Button>
-              )}
-            </Flex>
-          </form>
-          <Text>{hasil}</Text>
+          <Select
+            icon={<TriangleDownIcon />}
+            variant={"outline"}
+            placeholder={"Kepribadian"}
+          >
+            {Personalities.map((i: any) => (
+              <>
+                <option key={i.id} value={i.name}>
+                  {i.name}
+                </option>
+              </>
+            ))}
+          </Select>
+          <Button>Submit</Button>
         </VStack>
       </Flex>
     </>
